cbccd28f2d6fb6ce34157408c79578919ad21310
sergey <110@mobilon.ru>
2022-10-04 10:57:14 +0700

    fix

--- src/execute-workflow.ts
+++ src/execute-workflow.ts
@@ -38,13 +38,12 @@ app.listen(8000, () => {
 
 async function run() {
   console.log('start');
-  const connection = new Connection(); // Connect to localhost with default ConnectionOptions.
+  const connection = await Connection.connect();
+  // const connection = new Connection(); // Connect to localhost with default ConnectionOptions.
   // In production, pass options to the Connection constructor to configure TLS and other settings.
   // This is optional but we leave this here to remind you there is a gRPC connection being established.
 
-  const client = new WorkflowClient(connection.service, {
-    // In production you will likely specify `namespace` here; it is 'default' if omitted
-  });
+  const client = new WorkflowClient({connection});
 
   // Invoke the `example` Workflow, only resolved when the workflow completes
   const result = await client.start(example, {
